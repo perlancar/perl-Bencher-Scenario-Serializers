5a8564e44a8d112eaecae4a92c6b6e0c57de29c0
perlancar (@pc-home) <perlancar@gmail.com>
2015-10-14 17:16:53 +0700

    Add the rest of the serializers (yaml, storable, sereal, messagepack)

--- lib/Bencher/Scenario/Serializers.pm
+++ lib/Bencher/Scenario/Serializers.pm
@@ -59,8 +59,67 @@ our $scenario = {
             module => 'Pegex::JSON',
             code_template => 'state $obj = Pegex::JSON->new; $obj->load(<data>);',
         },
+
+        {
+            tags => ['yaml', 'serialize'],
+            fcall_template => 'YAML::Old::Dump(<data>)',
+        },
+        {
+            tags => ['yaml', 'deserialize'],
+            fcall_template => 'YAML::Old::Load(<data>)',
+        },
+        {
+            tags => ['yaml', 'serialize'],
+            fcall_template => 'YAML::Syck::Dump(<data>)',
+        },
+        {
+            tags => ['yaml', 'deserialize'],
+            fcall_template => 'YAML::Syck::Load(<data>)',
+        },
+        {
+            tags => ['yaml', 'serialize'],
+            fcall_template => 'YAML::XS::Dump(<data>)',
+        },
+        {
+            tags => ['yaml', 'deserialize'],
+            fcall_template => 'YAML::XS::Load(<data>)',
+        },
+
+        {
+            tags => ['binary', 'serialize', 'cant_handle_scalar'],
+            fcall_template => 'Storable::freeze(<data>)',
+        },
+        {
+            tags => ['binary', 'deserialize', 'cant_handle_scalar'],
+            fcall_template => 'Storable::thaw(<data>)',
+        },
+
+        {
+            tags => ['binary', 'serialize'],
+            fcall_template => 'Sereal::encode_sereal(<data>)',
+        },
+        {
+            tags => ['binary', 'deserialize'],
+            fcall_template => 'Sereal::decode_sereal(<data>)',
+        },
+
+        {
+            name => 'Data::MessagePack::pack',
+            tags => ['binary', 'serialize'],
+            module => 'Data::MessagePack',
+            function => 'pack',
+            code_template => 'state $obj = Data::MessagePack->new; $obj->pack(<data>)',
+        },
+        {
+            name => 'Data::MessagePack::unpack',
+            tags => ['binary', 'deserialize'],
+            module => 'Data::MessagePack',
+            function => 'unpack',
+            code_template => 'state $obj = Data::MessagePack->new; $obj->unpack(<data>)',
+        },
     ],
 
+    # XXX: add more datasets (larger data, etc)
     datasets => [
         {
             name => 'undef',
@@ -99,7 +158,7 @@ our $scenario = {
             summary => 'null',
             args => {data=>'null'},
             tags => ['deserialize'],
-            include_participant_tags => ['deserialize'],
+            include_participant_tags => ['json & deserialize'],
             exclude_participant_tags => ['cant_handle_scalar'],
         },
         {
@@ -107,7 +166,7 @@ our $scenario = {
             summary => 'A single number (-1.23)',
             args => {data=>-1.23},
             tags => ['deserialize'],
-            include_participant_tags => ['deserialize'],
+            include_participant_tags => ['json & deserialize'],
             exclude_participant_tags => ['cant_handle_scalar'],
         },
         {
@@ -115,7 +174,7 @@ our $scenario = {
             summary => 'A string 1024-character long',
             args => {data=>'"' . ('a' x 1024) . '"'},
             tags => ['deserialize'],
-            include_participant_tags => ['deserialize'],
+            include_participant_tags => ['json & deserialize'],
             exclude_participant_tags => ['cant_handle_scalar'],
         },
         {
@@ -123,7 +182,7 @@ our $scenario = {
             summary => 'A 10-element array containing ints',
             args => {data=>'[1,2,3,4,5,6,7,8,9,10]'},
             tags => ['deserialize'],
-            include_participant_tags => ['deserialize'],
+            include_participant_tags => ['json & deserialize'],
         },
     ],
 };
@@ -134,72 +193,3 @@ our $scenario = {
 =head1 SYNOPSIS
 
  % bencher -m Serializers [other options]...
-
-
-=head1 TEMP
-
-   'YAML::Old' => {
-        tags => ['yaml'],
-        serialize => sub {
-            YAML::Old::Dump($_[0]);
-        },
-        deserialize => sub {
-            YAML::Old::Load($_[0]);
-        },
-    },
-
-    'YAML::Syck' => {
-        tags => ['yaml'],
-        serialize => sub {
-            YAML::Syck::Dump($_[0]);
-        },
-        deserialize => sub {
-            YAML::Syck::Load($_[0]);
-        },
-    },
-
-    'YAML::XS' => {
-        tags => ['yaml'],
-        serialize => sub {
-            YAML::XS::Dump($_[0]);
-        },
-        deserialize => sub {
-            YAML::XS::Load($_[0]);
-        },
-    },
-
-    'Storable' => {
-        tags => ['binary', 'core'],
-        serialize => sub {
-            Storable::freeze($_[0]);
-        },
-        deserialize => sub {
-            Storable::thaw($_[0]);
-        },
-    },
-
-    'Sereal' => {
-        tags => ['binary'],
-        serialize => sub {
-            Sereal::encode_sereal($_[0]);
-        },
-        deserialize => sub {
-            Sereal::decode_sereal($_[0]);
-        },
-    },
-
-    'Data::MessagePack' => {
-        tags => ['binary'],
-        serialize => sub {
-            state $obj = Data::MessagePack->new;
-            $obj->pack($_[0]);
-        },
-        deserialize => sub {
-            state $obj = Data::MessagePack->new;
-            $obj->unpack($_[0]);
-        },
-    },
-);
-
-
-    ],
